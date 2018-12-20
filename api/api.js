2be33d5666505f4a78531f342d5ed813cc2b1617
Marcus Sund <38603709+Stronkerton@users.noreply.github.com>
2018-12-20 16:56:01 +0100

    Add files via upload

--- api/api.js
+++ api/api.js
@@ -4,4 +4,7 @@ const apiRouter = express.Router();
 const employeeRouter = require('./employees');
 apiRouter.use('/employees', employeeRouter);
 
+const menuRouter = require('./menus');
+apiRouter.use('/menus', menuRouter);
+
 module.exports = apiRouter;
